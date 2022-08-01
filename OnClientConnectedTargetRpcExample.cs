ba41b3a2cd3d6201122ea6def363649d9e0a37d1
Virosa <dracopm@hotmail.com>
2022-08-01 19:39:29 -0400

    Create OnClientConnectedTargetRpcExample.cs

---
+++ OnClientConnectedTargetRpcExample.cs
@@ -0,0 +1,68 @@
+using FishNet;
+using FishNet.Connection;
+using FishNet.Object;
+using FishNet.Transporting;
+using UnityEngine;
+
+public class OnClientConnectedTargetRpcExample : NetworkBehaviour
+    {
+        private static int playerNumber;
+
+        // I added these, because inside a "Match" instance, you would probably do something like this.
+        private NetworkConnection player1Connection;
+        private NetworkConnection player2Connection;
+        
+        private void Awake()
+        {
+            InstanceFinder.ServerManager.OnRemoteConnectionState += OnClientConnectionState;
+        }
+        
+        private void OnClientConnectionState(NetworkConnection connection, RemoteConnectionStateArgs arguments)
+        {
+            if (arguments.ConnectionState == RemoteConnectionState.Started) RegisterPlayer(connection);
+        }
+
+        private void RegisterPlayer(NetworkConnection connection)
+        {
+            var nextPlayerNumber = GetNextPlayerNumber();
+
+            switch (nextPlayerNumber)
+            {
+                case 1:
+                    player1Connection = connection;
+                    SendPlayerNumber(connection, 1);
+                    break;
+                
+                case 2:
+                    player2Connection = connection;
+                    SendPlayerNumber(connection, 2);
+                    break;
+                
+                default:
+                    SendNoGameForYou(connection);
+                    break;
+            }
+        }
+
+        private int GetNextPlayerNumber()
+        {
+            return ++playerNumber;
+        }
+
+        /// <summary>
+        /// A Broadcast would work better, especially since this still assume the client is an Observer of this NetworkObject.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="number"></param>
+        [TargetRpc]
+        private void SendPlayerNumber(NetworkConnection target, int number)
+        {
+            Debug.Log($"Server sent us our Player Number: {number}.");
+        }
+
+        [TargetRpc]
+        private void SendNoGameForYou(NetworkConnection target)
+        {
+            Debug.Log($"From Server: Computer says noooo.");
+        }
+    }
